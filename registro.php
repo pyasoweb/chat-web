064cdf5e573628198a4e67bbe00709bf8c81207c
dj calvin <laberinto692@gmail.com>
2026-05-26 23:10:50 -0400

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,23 +1,16 @@
 <?php
 $nombre = $_POST['nombre'];
-$email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
-$cpassword = $_POST['cpassword1'];
-$Lugar = $_POST ['textfield'];
-$select = $_POST ['select'];
-$select2 = $_POST ['select2'];
-$select3 = $_POST ['select3'];
-$select4 = $_POST ['select4'];
-$para = 'pyasowe@gmail.com';
+$para = 'pyasoweb@gmail.com';
 $titulo = 'ASUNTO DEL MENSAJE';
 $header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Password: $mensaje\n Rpassword: $mensaje\n Lugar: $Lugar\n Año: $select\n Mes: $select2\n Dia: $select3\n Sexo: $select4";
+$msjCorreo = "Nombre: $nombre\n Mensaje:\n $mensaje";
   
 if ($_POST['submit']) {
 if (mail($para, $titulo, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Mensaje enviado, muchas gracias.');
-window.location.href = 'https://chatalborada.com/';
+window.location.href = 'http://tokyo-zona-zero.esy.es';
 </script>";
 } else {
 echo 'Falló el envio';
