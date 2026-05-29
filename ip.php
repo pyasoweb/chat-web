61d15b22aa6241fa65fb957cafe8100672e1405f
latinoschats <laberinto692@gmail.com>
2026-05-29 17:35:27 -0400

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,9 +1,17 @@
-#php 7.x
-<?php if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    echo $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    echo $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    echo $ip = $_SERVER['REMOTE_ADDR'];
-}
+<?php
+echo "<h1>Estaditicas del sitio:</h1>";
+echo $_SERVER['SERVER_NAME'];
+ 
+//Recuperar informacion del visitante de la variabler globa $_SERVER
+echo "<h4>nombre de la pagina web actual</h4>";
+echo $_SERVER['PHP_SELF'];
+ 
+echo "<h4>Pagina web de donde viene el visitante</h4>";
+echo $_SERVER['HTTP_REFERER'];
+ 
+echo "<h4> Nombre del navegador</h4>";
+echo $_SERVER['HTTP_USER_AGENT'];
+ 
+echo "<h4>Direccion Ip del visitante</h4>";
+echo $_SERVER['REMOTE_ADDR'];
 ?>
