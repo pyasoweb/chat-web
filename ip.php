620f881a7822805d1c847db0c6fb1acc437d38d9
latinoschats <laberinto692@gmail.com>
2026-05-29 17:37:38 -0400

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Estaditicas del sitio:</h1>";
+echo "<h1>Estadisticas del sitio:</h1>";
 echo $_SERVER['SERVER_NAME'];
  
 //Recuperar informacion del visitante de la variabler globa $_SERVER
@@ -14,4 +14,13 @@ echo $_SERVER['HTTP_USER_AGENT'];
  
 echo "<h4>Direccion Ip del visitante</h4>";
 echo $_SERVER['REMOTE_ADDR'];
-?>
+ 
+//Utilizamos api de geolocalizacion
+$data = @file_get_contents("https://api.ipgeolocationapi.com/geolocate/" . $_SERVER['REMOTE_ADDR']);
+$items = json_decode($data, true);
+ 
+echo "<p>La visita se realiza desde : ";
+echo $items["continent"];
+echo ", ";
+echo $items["name"];
+echo "</p>";
