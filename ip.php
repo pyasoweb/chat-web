1d3ff5e7dd82e6362b03be04803401247eb3fcf9
latinoschats <laberinto692@gmail.com>
2026-05-30 16:47:20 -0400

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,13 +1,11 @@
-<?PHP
-// IP Address Logger
-/* to use this simply put this code into a php file named
-log.php, then either include it into your main page website
-or just direct the person to this page. make sure you have a
-log.txt file in your root directory of the server for it to save
-all ip logs too. */
-
-$ip = getenv("REMOTE_ADDR");
-$filename = "log.txt";
-$myfile = fopen($filename, "a+") or die("Couldn't open file");
-fwrite($myfile, $ip) or die("Couldn't write to file");
+<?php
+$Fichero = "archivo.txt"; //nombre del fichero donde se guardan los informes.
+$ip = $_SERVER["REMOTE_ADDR"]; //guarda en la variable el ip 
+$fecha = date("Y-m-d;H:i:s"); //fecha y hora (por lo general del servidor) 
+$sistema = $_SERVER['HTTP_USER_AGENT']; //Esto nos genera varios datos del navegador y del sistema operativo 
+$conproxy = $_SERVER["HTTP_X_FORWARDED_FOR"]; //En caso de usar proxy para esconderse aqui estaria el ip real
+$log = "FECHA: $fecha SISTEMA: $sistema IP: $ip IPPROXY: $conproxy \x0D\x0A"; 
+$fp = fopen($Fichero, "a" ); 
+fwrite($fp, $log); 
+fclose($fp); 
 ?>
