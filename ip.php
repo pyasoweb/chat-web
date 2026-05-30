848070ba39183242b233e2c732b5984aef2ceb22
latinoschats <laberinto692@gmail.com>
2026-05-30 09:58:22 -0400

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,17 @@
+<?php
+function getIP() {
+   if (isset($_SERVER)) {
+      if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+         return $_SERVER['HTTP_X_FORWARDED_FOR'];
+      } else {
+         return $_SERVER['REMOTE_ADDR'];
+      }
+   } else {
+      if (isset($GLOBALS['HTTP_SERVER_VARS']['HTTP_X_FORWARDER_FOR'])) {
+         return $GLOBALS['HTTP_SERVER_VARS']['HTTP_X_FORWARDED_FOR'];
+      } else {
+         return $GLOBALS['HTTP_SERVER_VARS']['REMOTE_ADDR'];
+      }
+   }
+}
+?>
